95930403149c8796dd3d578c89ce62332e64c87e
Gonzalo Chiappara <gchiappara@scanntech.com>
2017-02-06 12:42:01 -0300

    primer commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		if (llueve)
+			"Enfrente"
+		else if (lunes)
+			"Tres Cruces"
+		else if (dia <= "Miercoles")
+			"Panaderia", "Al lado Panaderia"
+		else
+			"Panaderia"
+		*/
+
+	}
+
+}
