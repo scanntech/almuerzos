7b3044900e3d03bfc9ecbefc03e1e4fa28b4bbb4
benkla <cbenitez@scanntech.com>
2017-02-07 15:29:49 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,16 +3,21 @@ public class Main {
 	public static void main(String[] args) {
 		/* 
 		//Hay que intentar contemplar todos los casos
-		if (pintó hacer otra cosa){
+		//Ya somos grandes y nos bancamos los tildes en el código
+		Calendar cal = Calendar.getInstance();
+        	int dia = cal.get(Calendar.DAY_OF_WEEK);
+        	int lunes = Calendar.MONDAY;
+        	int miércoles = Calendar.WEDNESDAY;
+		if (pintóHacerOtraCosa()) {
 		 	System.out.println("Vamos a donde pintó");
-		} else if (llueve) {
-			System.out.println("Delivery pidiendo con tiempo");		
-		} else if (lunes) {
-			System.out.println("Tres Cruces");
-		} else if (dia <= "Miercoles") {
-			System.out.println("Panaderia,Al lado Panaderia");
+		} else if (tiempo.getClima().equalsIgnoreCase("lluvia")) {
+			System.out.println("Delivery pidiendo con tiempo");			
+		} else if (dia == lunes) {		
+			System.out.println("Tres Cruces");			
+		} else if (dia <= miércoles) {
+			System.out.println("Panadería, Al lado Panadería");
 		} else {
-			System.out.println("Panaderia");
+			System.out.println("Panadería");
 		}
 		*/
 
